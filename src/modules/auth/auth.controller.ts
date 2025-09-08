ad9add97742d8b2b640daa9d8a54aff1b0ef8a7b
Phyo Khant Kyaw <www.pkkcobra01@gmail.com>
2025-09-08 20:16:45 +0700

    [PKK] Verify Token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SuccessResponse } from 'src/common/exceptions/success';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -13,5 +22,11 @@ export class AuthController {
   ): Promise<SuccessResponse> {
     const accessToken = await this.authService.signIn(loginDto);
     return new SuccessResponse(accessToken);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('verify-token')
+  validate(@Req() req) {
+    return new SuccessResponse(req.user);
   }
 }
