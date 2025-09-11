fda6b5ddeb065079252fb768cdd9e55f1f5bda43
Phyo Khant Kyaw <www.pkkcobra01@gmail.com>
2025-09-11 19:55:05 +0700

    [PKK] Fix Course Update

--- src/modules/courses/dto/update-course.dto.ts
+++ src/modules/courses/dto/update-course.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+  @IsOptional()
+  @IsString()
+  image?: string;
+}
